adf1afd1787112a61057842b3f28fa24d20750c1
Theresia Calista <theresiacalista57@gmail.com>
2025-04-06 15:30:57 +0700

    exercise 1

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,30 @@
-<script></script>
+<script>
+import {defineNuxtComponent} from "#app"
+
+export default defineNuxtComponent({
+  data: () => ({
+    photoGallery: []
+  }),
+  methods: {
+    fetchPhotoGallery(){
+      fetch('https://jsonplaceholder.typicode.com/photos') 
+        .then(response => {
+          this.photoGallery = response.json()
+        })
+    }
+  }
+})
+</script>
 
 <template>
   <h1>Photo Gallery</h1>
+  <button @click="fetchPhotoGallery">Ftech Data</button>
+  <ul>
+    <li v-for="photo in photoGallery" :key="`photo-id-${photo.id}`">
+      <img :src="photo-url" />
+    </li>
+  </ul>
 </template>
 
 <style></style>
+<Script
