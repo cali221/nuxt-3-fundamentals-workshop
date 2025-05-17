b59cfaaeffe9b5d60c927792d02f2dc3d33c4f19
Theresia Calista <theresiacalista57@gmail.com>
2025-05-17 15:15:12 +0700

    added google font to pages/index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -5,7 +5,9 @@
     <Html>
         <Head>
             <Title>Water Bottle</Title>
-            <Link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css" />
+            <Link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <Link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <Link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
         </Head>
     </Html>
     <NuxtLayout>
